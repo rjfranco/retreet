06f9e4f0dda112bce8c20f7eaddfd80d422f3ebb
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-05 14:42:37 -0700

    Initializing beginnings of new sample_app for rails tutorial

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+gem 'sass'
+
+# Use unicorn as the web server
+gem 'unicorn'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+
+group :development do
+    gem 'rspec-rails'
+end
+
+group :test do
+    gem 'rspec'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'de70ef9558fb3db7c154bec855b5978c2bfd576706804a1cd968be9d983362155836cb7326e5d0c72ee3396b477eaedb0ab8b06942725707ad7185d706fae821'
