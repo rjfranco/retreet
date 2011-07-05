f2b1e1949c79bf6c5b06b18e9b9288465309b985
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-05 16:49:30 -0700

    Removing superfluous files, and adding about page, as well as testing for the about page.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,5 +15,12 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 
 end
