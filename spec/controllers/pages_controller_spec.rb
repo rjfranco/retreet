73a1e941e5fae8c7fef65c90409d25edc3d1f7d3
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-06 16:46:38 -0700

    updating controller spec to more intelligently check for titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+      @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
     
     it "should have the right title" do
         get 'home'
-        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+        response.should have_selector("title", :content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -29,7 +33,7 @@ describe PagesController do
     
     it "should have the right title" do
         get 'contact'
-        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+        response.should have_selector("title", :content => "#{@base_title} | Contact")
     end
   end
   
@@ -41,7 +45,7 @@ describe PagesController do
     
     it "should have the right title" do
         get 'about'
-        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+        response.should have_selector("title", :content => "#{@base_title} | About")
     end
   end
 
