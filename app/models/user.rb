0c35eb90e549839a6be03d4c90e934bd23a8316a
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-08 13:51:12 -0700

    Adding new user column for encrypted passwords, and related tests for the column.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,22 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   validates :name, :presence => true, :length => { :maximum => 50 }
   validates :email, :presence => true, :email => true, :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
 end
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
