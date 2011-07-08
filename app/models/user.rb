6852db361b2d674d96fbe354b46b03cf408d02c8
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-08 09:48:01 -0700

    Adding new :email validator method using non-regex method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email
   
-  email_regex = /\A[\w\d+.\-]+@[a-z\-\d]+\.[a-z.\-]+\z/i
-  
   validates :name, :presence => true, :length => { :maximum => 50 }
-  validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true, :email => true, :uniqueness => { :case_sensitive => false }
 end
 
 # == Schema Information
