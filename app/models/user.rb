c7a98813ac992bacf8dc376b1f6e0404fec49db6
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-08 14:45:35 -0700

    Adding user functions for encryption / storage of passwords, and related tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,36 @@ class User < ActiveRecord::Base
   validates :name, :presence => true, :length => { :maximum => 50 }
   validates :email, :presence => true, :email => true, :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }
+  
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+  
+  def User.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
 
 
