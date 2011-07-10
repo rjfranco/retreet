1f0fd9c8e052229011f349e4482be58e7c5ce60d
Ramiro Franco <rjfranco@gmail.com>
2011-07-10 12:12:41 -0700

    Adding pages for new user session (generated by rails), and making changes to the view to link to signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
+
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/singout', :to => 'sessions#destroy'
+  
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
