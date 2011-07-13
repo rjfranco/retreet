c727fc6a8d7dbc212993ff9f0aae17e9cf61c463
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-13 10:34:48 -0700

    Creating initial files, and methods for a "Settings" page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -101,4 +101,26 @@ describe UsersController do
       end
     end
   end
+  
+  describe "Get 'edit'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector('title', :content => "Edit user")
+    end
+    
+    it "should have a link to edit your gravatar" do
+      get :edit, :id => @user
+      response.should have_selector('a', :href => 'http://gravatar.com/emails', :content => 'Edit your Gravatar')
+    end
+  end
 end
