312cca81ac179231655680f79a23f3183b131943
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-15 13:21:49 -0700

    Adding a basic security model to make sure users are only allowed to access their own edit pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   def new
     @user = User.new
@@ -40,5 +41,10 @@ class UsersController < ApplicationController
   private
     def authenticate
        deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,11 +18,29 @@ module SessionsHelper
   
   def sign_out
     cookies.delete(:remember_token)
-    current_user = nil    
+    current_user = nil
+  end
+  
+  def current_user?(user)
+    user == current_user
   end
   
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  def store_location
+    session[:return_to] = request.fullpath    
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
+  def clear_return_to
+    session[:return_to] = nil
   end
   
   private
