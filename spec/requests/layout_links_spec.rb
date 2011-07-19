8f535c37c0528f9703966985329973e6adf40397
Ramiro Jr. Franco <rjfranco@gmail.com>
2011-07-19 16:47:11 -0700

    Adding a User index, and related menu / style items.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -76,5 +76,10 @@ describe "LayoutLinks" do
       visit root_path
       response.should have_selector("a", :href => edit_user_path(@user), :content => "Settings")
     end
+    
+    it "should have a users link" do
+      visit root_path
+      response.should have_selector("a", :href => users_path, :content => "Users")
+    end
   end
 end
