8a1c48caedd128a6e4c6c12bd0526744fa1379cf
Ramiro Franco <rjfranco@gmail.com>
2011-07-24 18:02:52 -0700

    Adding micro post validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -24,6 +24,20 @@ describe Micropost do
       @micropost.user.should == @user
     end
   end
+  
+  describe "validations" do
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    
+    it "should require non-blank content" do
+      @user.microposts.build(:content => '          ').should_not be_valid
+    end
+    
+    it "should reject content that is too long"  do
+      @user.microposts.build(:content => 'a' * 141).should_not be_valid
+    end
+  end
 end
 
 # == Schema Information
