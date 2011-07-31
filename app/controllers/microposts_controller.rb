06b2af0fcf752c6819cae2f0e1d44863c887a9b3
Ramiro Franco <rjfranco@gmail.com>
2011-07-31 16:52:16 -0700

    Committing base for "feed" model, needs more work. Added functionality for removing your own posts, and modified display for the home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,15 +1,25 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate
+  before_filter :authorized_user, :only => :destroy
   
   def create
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       redirect_to root_path, :flash => { :success => 'Your Post Has Been Succesfully Created.'}
     else
+      @feed_items = []
       render 'pages/home'
     end
   end
   
   def destroy
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => 'Post has been deleted.'}
   end
+  
+  private
+    def authorized_user
+      @micropost = Micropost.find(params[:id])
+      redirect_to root_path unless current_user?(@micropost.user)
+    end
 end
--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -59,4 +59,34 @@ describe MicropostsController do
       end
     end
   end
+  
+  describe "POST 'destroy'" do
+    describe "for an unauthorized user" do
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        @micropost = Factory(:micropost, :user => @user)
+        test_sign_in(wrong_user)
+      end
+      
+      it "should deny access" do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+    end
+    
+    describe "for an authorized user" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @micropost = Factory(:micropost, :user => @user)
+      end
+      
+      it "should destroy the micropost" do
+        lambda do
+          delete :destroy, :id => @micropost
+          response.should redirect_to(root_path)
+        end.should change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
